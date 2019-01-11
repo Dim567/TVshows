ea5c4f4846210093b6107f3820a1861d206d6123
Dmitry Matyunin <dmitry.matyunin.firefly@gmail.com>
2019-01-11 17:20:21 +0200

    refactor pagination with handleClick method

--- src/Pagination.js
+++ src/Pagination.js
@@ -7,21 +7,35 @@ function mapStateToProps(state){
 }
 
 class Pagination extends Component{
+	handleClick(type,aPage,ind=1){
+		const page=aPage-10>=1?aPage-10:1;
+		switch(type){
+			case 'back':
+				this.props.dispatch((dispatch)=>remoteRequest(dispatch,page));
+				break;
+			case 'pages':
+				this.props.dispatch((dispatch)=>remoteRequest(dispatch,ind+aPage-(aPage-1)%10));
+				break;
+			case 'forward':
+				this.props.dispatch((dispatch)=>remoteRequest(dispatch,aPage+10));
+				break;
+			default:
+				break;
+		}
+	}
 	render() {
+		let aPage=this.props.activePage;
 		return (
 			<div className="pagContainer">
-				<div className='Pagination' onClick={()=>alert('back')}>{'<<'}</div>
+				<div className='Pagination' onClick={()=>this.handleClick('back',aPage)}>{'<<'}</div>
 				{Array(10).fill(1).map((el,ind)=>
-					<div className={this.props.activePage===(ind+1)?'activePagination':'Pagination'} key={'key'+ind} onClick={
-						()=>{this.props.dispatch(
-							(dispatch)=>remoteRequest(dispatch,ind+1)
-							);
-						}
+					<div className={aPage===(ind+1+(aPage-1)-(aPage-1)%10)?'activePagination':'Pagination'} key={'key'+ind} onClick={
+						()=>this.handleClick('pages',aPage,ind)
 					}>
-						{ind+1}
+						{ind+1+(aPage-1)-(aPage-1)%10}
 					</div>
 				)}
-				<div className='Pagination' onClick={()=>alert('forward')}>{'>>'}</div>
+				<div className='Pagination' onClick={()=>this.handleClick("forward",aPage)}>{'>>'}</div>
 			</div>
 		);
 	}
