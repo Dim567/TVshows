dff80232a86256cc8d7c1e97cbe4411341f8fbe0
Dmitry Matyunin <dmitry.matyunin.firefly@gmail.com>
2019-01-28 10:59:45 +0200

    Updates

---
+++ precache-manifest.6d0e7579e79b7564cdd55c50c1078da9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ac0efe2c453e965f082",
+    "url": "/TVshows/static/js/runtime~main.0ac0efe2.js"
+  },
+  {
+    "revision": "81bec1cec1868c49fd36",
+    "url": "/TVshows/static/js/main.81bec1ce.chunk.js"
+  },
+  {
+    "revision": "39b4de9a80754c54280c",
+    "url": "/TVshows/static/js/1.39b4de9a.chunk.js"
+  },
+  {
+    "revision": "81bec1cec1868c49fd36",
+    "url": "/TVshows/static/css/main.e66dfad5.chunk.css"
+  },
+  {
+    "revision": "0d2744f07ce0b65f7c3715241cb6e15e",
+    "url": "/TVshows/index.html"
+  }
+];
